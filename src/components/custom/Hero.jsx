284b3daacd1de52cf229a9f771457ac3ac343fa5
Somi14 <somigour2002@gmail.com>
2024-10-31 01:05:42 +0530

    my changes

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -7,7 +7,7 @@ function Hero() {
         < div className='flex flex-row'>
         <div className='flex flex-col items-center mx-56 gap-9'>
             <h1 className='font-extrabold text-[60px] text-center mt-16'>
-                Plan Your Next Adventure with AI and team chaitali: <span className='text-blue-500'> Personalize Your Trip</span>
+                Plan Your Next Adventure with AI and team : <span className='text-blue-500'> Personalize Your Trip</span>
             </h1>
             <p className='text-xl text-gray-500 text-center'>AI Trip Planner is an intelligent travel assistant that helps you create personalized itineraries, making your travel planning easy and efficient. Discover new destinations and customize your trips with just a few clicks.</p>
               
